5c5c42efcb11f8dc1d4b044cd6cd4b0aaab9281b
Saleh Albatati <0xack13@gmail.com>
2016-09-24 21:04:47 +0300

    Subscription to event

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, Stat
 // This method is called when your extension is activated. Activation is
 // controlled by the activation events defined in package.json.
 export function activate(context: ExtensionContext) {
-
+    /* We will subsctibe an event rather than using shortcuts
     // Use the console to output diagnostic information (console.log) and errors (console.error).
     // This line of code will only be executed once when your extension is activated.
     console.log('Congratulations, your extension "WordCount" is now active!');
@@ -19,7 +19,19 @@ export function activate(context: ExtensionContext) {
 
     // Add to a list of disposables which are disposed when this extension is deactivated.
     context.subscriptions.push(wordCounter);
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);*/
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error).
+    // This line of code will only be executed once when your extension is activated.
+    console.log('Congratulations, your extension "WordCount" is now active!');
+
+    // create a new word counter
+    let wordCounter = new WordCounter();
+    let controller = new WordCounterController(wordCounter);
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(controller);
+    context.subscriptions.push(wordCounter);
 }
 
 class WordCounter {
@@ -47,7 +59,8 @@ class WordCounter {
             let wordCount = this._getWordCount(doc);
 
             // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            //this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
             this._statusBarItem.show();
         } else { 
             this._statusBarItem.hide();
@@ -71,5 +84,35 @@ class WordCounter {
 
     dispose() {
         this._statusBarItem.dispose();
+    }
+}
+
+class WordCounterController {
+
+    private _wordCounter: WordCounter;
+    private _disposable: Disposable;
+
+    constructor(wordCounter: WordCounter) {
+        this._wordCounter = wordCounter;
+        this._wordCounter.updateWordCount();
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        // update the counter for the current file
+        this._wordCounter.updateWordCount();
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._wordCounter.updateWordCount();
     }
 }
